9642c0710ea49e756d0cf66b39b922b027434864
Swirl86 <swesuzzi@gmail.com>
2024-11-20 12:23:29 +0100

    added app icon and splash screen

--- app/src/main/java/com/swirl/anime_hub/MainActivity.kt
+++ app/src/main/java/com/swirl/anime_hub/MainActivity.kt
@@ -3,6 +3,7 @@ package com.swirl.anime_hub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.swirl.anime_hub.ui.navigation.AnimeNavGraph
 import com.swirl.anime_hub.ui.theme.AnimeHubTheme
@@ -13,6 +14,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+
         setContent {
             AnimeHubTheme {
                 val navController = rememberNavController()
