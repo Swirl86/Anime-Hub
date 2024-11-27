9691572f2cfc64d190a379488e74974870efe21c
Swirl86 <swesuzzi@gmail.com>
2024-11-27 10:53:56 +0100

    cleanup and splashscreen bg color change

--- app/src/main/java/com/swirl/anime_hub/MainActivity.kt
+++ app/src/main/java/com/swirl/anime_hub/MainActivity.kt
@@ -3,11 +3,7 @@ package com.swirl.anime_hub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.navigation.compose.rememberNavController
 import com.swirl.anime_hub.ui.navigation.Navigation
 import com.swirl.anime_hub.ui.theme.AnimeHubTheme
 import dagger.hilt.android.AndroidEntryPoint
--- app/src/main/java/com/swirl/anime_hub/ui/navigation/Navigation.kt
+++ app/src/main/java/com/swirl/anime_hub/ui/navigation/Navigation.kt
@@ -1,15 +1,14 @@
 package com.swirl.anime_hub.ui.navigation
 
-import android.util.Log
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.getValue
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
